e07b535a61573a985f3273659faae0dde5d653c5
John <john.akonya@gmail.com>
2020-03-28 22:36:40 +0300

    Editing tasks

---
+++ vuecrudapp/src/views/Edit.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Edit task</h1>
+    <task-form @createOrUpdate="createOrUpdate" :task="this.task"></task-form>
+  </div>
+</template>
+
+<script>
+import taskForm from "../components/Taskform.vue";
+import { api } from "../helpers/helpers";
+
+export default {
+  name: "edit",
+  components: {
+    "task-form": taskForm
+  },
+  data: function() {
+    return {
+      task: {}
+    };
+  },
+  methods: {
+    createOrUpdate: async function(task) {
+      await api.updatetask(task);
+      this.flash("task updated successfully!", "success");
+      this.$router.push(`/tasks/${task._id}`);
+    }
+  },
+  async mounted() {
+    this.task = await api.gettask(this.$route.params.id);
+  }
+};
+</script>
+
+<style></style>
