ee2ceda5c082913af59a20fa733fa7ef80f65938
John <john.akonya@gmail.com>
2020-03-28 22:28:27 +0300

    made changes to this file

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ mongoose.connect("mongodb://localhost:27017/Vuecrudapp", {
   useNewUrlParser: true
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2007;
 const app = express();
 
-app.use(cors());
+app.use(cors("*"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
